62e11236d800de6c681bd96446cbb00859c1bcb1
FRECTER Don <poyertytheminecrafter@gmail.com>
2020-02-21 17:28:27 +0800

    added MineField class; now can generate random field of mines

---
+++ Engine/MineField.cpp
@@ -0,0 +1,32 @@
+#include "MineField.h"
+#include "Vec2.h"
+#include <assert.h>
+#include <random>
+
+void MineField::Tile::SpawnMine() {
+	assert(!hasMine);
+	hasMine = true;
+}
+
+bool MineField::Tile::CheckMine() const {
+	return hasMine;
+}
+
+MineField::MineField(int mineNum) {
+	assert(mineNum > 0 && mineNum < width * height);
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> xDist(0, width - 1);
+	std::uniform_int_distribution<int> yDist(0, height - 1);
+	for (int i = 0; i < mineNum; i++) {
+		Vec2 spawnPos;
+		do
+			spawnPos = { xDist(rng),yDist(rng) };
+		while (TileAtPos(spawnPos).CheckMine());
+		TileAtPos(spawnPos).SpawnMine();
+	}
+}
+
+MineField::Tile& MineField::TileAtPos(const Vec2& pos) {
+	return field[pos.y * width + pos.x];
+}
---
+++ Engine/MineField.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class MineField {
+private:
+	class Tile {
+	private:
+		enum class State {
+			Hidden,
+			Flag,
+			Open
+		};
+	public:
+		void SpawnMine();
+		bool CheckMine() const;
+	private:
+		State state = State::Hidden;
+		bool hasMine = false;
+	};
+public:
+	MineField(int mineNum);
+private:
+	Tile& TileAtPos(const Vec2& pos);
+private:
+	static constexpr int width = 10;
+	static constexpr int height = 10;
+	Tile field[width * height];
+};
